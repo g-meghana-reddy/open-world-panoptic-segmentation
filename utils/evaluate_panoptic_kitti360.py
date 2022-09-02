26653a15369a0341cc1838eed2325d70a85dc938
Anirudh S Chakravarthy <anirudh.s.chakravarthy@gmail.com>
2022-09-02 14:48:59 -0400

    Remove pdb

--- utils/evaluate_panoptic_kitti360.py
+++ utils/evaluate_panoptic_kitti360.py
@@ -184,7 +184,6 @@ if __name__ == '__main__':
 
     u_label_sem_class = class_lut[label & 0xFFFF]  # remap to xentropy format
     u_label_inst = label >> 16
-    import pdb; pdb.set_trace()
     if FLAGS.limit is not None:
       u_label_sem_class = u_label_sem_class[:FLAGS.limit]
       u_label_sem_cat = u_label_sem_cat[:FLAGS.limit]
@@ -200,7 +199,6 @@ if __name__ == '__main__':
       u_pred_inst = u_pred_inst[:FLAGS.limit]
 
     # class_evaluator.addBatch(u_pred_sem_class, u_pred_inst, u_label_sem_class, u_label_inst)
-    import pdb; pdb.set_trace()
     class_evaluator.addBatch(label_file.split('/')[-3], u_pred_sem_class, u_pred_inst, u_label_sem_class, u_label_inst)
 
   print("100%")
