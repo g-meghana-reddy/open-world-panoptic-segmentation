e33f660c5be96cf72257809b93b9b3f8783dbf1a
Anirudh S Chakravarthy <anirudh.s.chakravarthy@gmail.com>
2022-10-22 00:21:16 -0400

    Update dataset structure

--- segment_classifier/dataset/segment_data_test.py
+++ segment_classifier/dataset/segment_data_test.py
@@ -1,23 +1,25 @@
-from segment_dataset import SegmentDataset
-import numpy as np
-import os
 import torch
+import time
+from tqdm import tqdm
 
+from segment_dataset import SegmentDataset
 
 
 if __name__ == '__main__':
-
-    train_dataset = SegmentDataset('segment_dataset', 'training')
+    data_dir = "/project_data/ramanan/achakrav/4D-PLS/segment_classifier/segment_dataset"
+    train_dataset = SegmentDataset(data_dir, split='training')
     train_loader = torch.utils.data.DataLoader(
         train_dataset,
-        batch_size=1,
+        batch_size=32,
         shuffle=False,
-        num_workers=1,
+        num_workers=4,
         pin_memory=True,
         sampler=None,
-        drop_last=True, 
-        collate_fn=lambda x: x)
+        collate_fn=train_dataset.collate_batch)
 
-    for i, batch in enumerate(train_loader):
-        import pdb;pdb.set_trace()
-
+    start_time = time.time()
+    for i, batch in tqdm(enumerate(train_loader)):
+        # import pdb; pdb.set_trace()
+        pass
+    end_time = time.time()
+    print("Time taken:", end_time-start_time)
