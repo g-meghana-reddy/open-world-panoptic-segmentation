fc4d3d4b0a5ac0ee887e6b8e86be61989116e2a8
Anirudh S Chakravarthy <anirudh.s.chakravarthy@gmail.com>
2022-10-23 13:39:28 -0400

    Allow val metric and improve dataset class

---
+++ segment_classifier/dataset/generate_dataset.sh
@@ -0,0 +1,11 @@
+python generate_segment_dataset.py -g 1 -s 0 &
+python generate_segment_dataset.py -g 2 -s 1 &
+python generate_segment_dataset.py -g 3 -s 2 &
+python generate_segment_dataset.py -g 4 -s 3 &
+python generate_segment_dataset.py -g 5 -s 4 &
+python generate_segment_dataset.py -g 5 -s 5 &
+python generate_segment_dataset.py -g 6 -s 6 &
+python generate_segment_dataset.py -g 6 -s 7 &
+python generate_segment_dataset.py -g 7 -s 8 &
+python generate_segment_dataset.py -g 7 -s 9 &
+# python generate_segment_dataset.py -g 7 -s 10 &
--- segment_classifier/dataset/segment_dataset.py
+++ segment_classifier/dataset/segment_dataset.py
@@ -1,40 +1,69 @@
-import os
 import glob
+import os
+
 import numpy as np
+import torch
+from torch.utils.data import Dataset
 
 
-class SegmentDataset:
+class SegmentDataset(Dataset):
     def __init__(self, dataset_path, split='training', n_points=1024):
         self.path = dataset_path
         self.split = split
         self.n_points = n_points
-        self.segment_files = self.load_paths()
+
+        if split == "training":
+            self.sequences = ["{:02d}".format(i) for i in range(11) if i != 8]
+        elif split == "validation":
+            self.sequences = ["{:02d}".format(8)]
+        else:
+            assert False, "Split not implemented"
+
+        # compute weights for sampling during training
+        self.paths = self.load_paths()
+        if self.split == "training":
+            indices_file = os.path.join(dataset_path, "class_indices.npz")
+            if os.path.exists(indices_file):
+                print("Loading precomputed indices")
+                indices = dict(np.load(indices_file))
+                pos_indices, neg_indices = indices["pos_indices"], indices["neg_indices"]
+            else:
+                print("Precomputing indices, one-time only!")
+                pos_indices, neg_indices = [], []
+                for idx, path in enumerate(self.paths):
+                    segment_data = dict(np.load(path))
+                    if segment_data["gt_label"] == 1:
+                        pos_indices.append(idx)
+                    else:
+                        neg_indices.append(idx)
+                np.savez(
+                    indices_file,
+                    pos_indices=pos_indices,
+                    neg_indices=neg_indices
+                )
+
+            num_segments = len(self.paths)
+            self.weights = torch.zeros(num_segments)
+            self.weights[pos_indices] = num_segments / len(pos_indices)
+            self.weights[neg_indices] = num_segments / len(neg_indices)
 
     def load_paths(self):
         paths = []
-        for seq in os.listdir(self.path):
-            seq_dir = os.path.join(self.path, seq)
-            if not os.path.isdir(seq_dir):
-                continue
-            if self.split == 'training' and seq != '08':
-                paths.extend([
-                    file for file in sorted(glob.glob(os.path.join(seq_dir, "*.npz")))
-                ])
-            elif self.split == 'validation' and seq == '08':
-                paths.extend([
-                    file for file in sorted(glob.glob(os.path.join(seq_dir, "*.npz")))
-                ])
+        for seq in self.sequences:
+            paths.extend([
+                file for file in sorted(glob.glob(os.path.join(self.path, seq, "*.npz")))
+            ])
         return paths
 
     def __len__(self):
         """
         Return the length of dataset
         """
-        return len(self.segment_files)
+        return len(self.paths)
 
     def __getitem__(self, index):
-        segment_data = dict(np.load(self.segment_files[index]))
-        
+        segment_data = dict(np.load(self.paths[index]))
+
         # subsample / repeat points
         num_points = len(segment_data["xyz"])
         if num_points > self.n_points:
@@ -51,7 +80,6 @@ class SegmentDataset:
                 segment_data["semantic_features"] = np.vstack([
                     segment_data["semantic_features"], np.zeros((residual, 256))
                 ])
-
             chosen_idxs = np.arange(0, self.n_points, dtype=np.int32)
 
         np.random.shuffle(chosen_idxs)
@@ -68,13 +96,16 @@ class SegmentDataset:
 
         for key in batch[0].keys():
             if isinstance(batch[0][key], np.ndarray):
-                ans_dict[key] = np.concatenate([batch[k][key][np.newaxis, ...] for k in range(batch_size)], axis=0)
-
+                # skip string arrays since torch can't handle it
+                if batch[0][key].dtype.kind in ("U", "S"):
+                    continue
+                ans_dict[key] = torch.cat(
+                    [torch.from_numpy(batch[k][key][np.newaxis, ...]) for k in range(batch_size)], dim=0
+                )
             else:
                 ans_dict[key] = [batch[k][key] for k in range(batch_size)]
                 if isinstance(batch[0][key], int):
-                    ans_dict[key] = np.array(ans_dict[key], dtype=np.int32)
+                    ans_dict[key] = torch.tensor(ans_dict[key], dtype=torch.int)
                 elif isinstance(batch[0][key], float):
-                    ans_dict[key] = np.array(ans_dict[key], dtype=np.float32)
-
+                    ans_dict[key] = torch.tensor(ans_dict[key], dtype=torch.float32)
         return ans_dict
