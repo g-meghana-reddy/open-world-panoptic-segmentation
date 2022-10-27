094a7a9e746cefe8919ef05cc0332ecac0e31c97
g-meghana-reddy <meghana97g@gmail.com>
2022-10-27 15:10:24 -0400

    Added data augmentation

--- hu-segmentation/segment_classifier/dataset/segment_dataset.py
+++ hu-segmentation/segment_classifier/dataset/segment_dataset.py
@@ -7,11 +7,18 @@ from torch.utils.data import Dataset
 
 
 class SegmentDataset(Dataset):
-    def __init__(self, dataset_path, split='training', n_points=1024, num_things=8):
+    def __init__(self, dataset_path, split='training', n_points=1024, num_things=8, semantic_classes=20):
         self.path = dataset_path
         self.split = split
         self.n_points = n_points
         self.num_things = num_things
+        self.semantic_classes = semantic_classes
+
+        # data augmentation flags
+        self.random_jitter = False
+        self.random_flip = False
+        self.random_shuffle = True
+        self.rotate_to_center = True
 
         if split == "training":
             self.sequences = ["{:02d}".format(i) for i in range(11) if i != 8]
@@ -32,14 +39,14 @@ class SegmentDataset(Dataset):
             else:
                 print("Precomputing indices, one-time only!")
                 pos_indices, neg_indices = [], []
-                sem_class_counts = np.zeros((self.num_things, 1))
+                sem_class_counts = np.zeros((self.semantic_classes, 1))
                 for idx, path in enumerate(self.paths):
                     segment_data = dict(np.load(path))
                     if segment_data["gt_label"] == 1:
                         pos_indices.append(idx)
                     else:
                         neg_indices.append(idx)
-                    sem_class_counts[segment_data["semantic_label"] - 1] += 1
+                    sem_class_counts[segment_data["semantic_label"]] += 1
                 np.savez(
                     indices_file,
                     pos_indices=pos_indices,
@@ -52,8 +59,8 @@ class SegmentDataset(Dataset):
             self.weights[pos_indices] = num_segments / len(pos_indices)
             self.weights[neg_indices] = num_segments / len(neg_indices)
             self.sem_weights = np.divide(
-                sem_class_counts.sum(), sem_class_counts, 
-                out=np.zeros_like(sem_class_counts), where=sem_class_counts != 0)
+                sem_class_counts[1:9].sum(), sem_class_counts[1:9], 
+                out=np.zeros_like(sem_class_counts[1:9]), where=sem_class_counts[1:9] != 0)
 
     def load_paths(self):
         paths = []
@@ -78,9 +85,10 @@ class SegmentDataset(Dataset):
             chosen_idxs = np.random.choice(np.arange(num_points), self.n_points, replace=False)
         # TODO: something smarter than this
         else:
-            chosen_idxs = np.arange(0, self.n_points, dtype=np.int32)
+            chosen_idxs = np.arange(0, num_points, dtype=np.int32)
             if num_points < self.n_points:
-                extra_idxs = np.random.choice(chosen_idxs, self.npoints - len(chosen_idxs), replace=True)
+                extra_idxs = np.random.choice(
+                    chosen_idxs, self.n_points - len(chosen_idxs), replace=True)
                 chosen_idxs = np.concatenate([chosen_idxs, extra_idxs], axis=0)
                 # residual = self.n_points - num_points
                 # segment_data["indices"] = np.concatenate([
@@ -92,15 +100,44 @@ class SegmentDataset(Dataset):
                 #     segment_data["semantic_features"], np.zeros((residual, 256))
                 # ])
 
-        np.random.shuffle(chosen_idxs)
-        segment_data["indices"] = segment_data["indices"][chosen_idxs]
-        segment_data["first_frame_xyz"] = segment_data["first_frame_xyz"][chosen_idxs]
-        segment_data["xyz"] = segment_data["xyz"][chosen_idxs]
-        segment_data["semantic_features"] = segment_data["semantic_features"][chosen_idxs]
+        # Data augmentation
+        if self.random_shuffle:
+            np.random.shuffle(chosen_idxs)
+            segment_data["indices"] = segment_data["indices"][chosen_idxs]
+            segment_data["first_frame_xyz"] = segment_data["first_frame_xyz"][chosen_idxs]
+            segment_data["xyz"] = segment_data["xyz"][chosen_idxs]
+            segment_data["semantic_features"] = segment_data["semantic_features"][chosen_idxs]
 
-        # Perform any data augmentation?
+        segment_data["first_frame_xyz"] = self.points_augmentation(segment_data["first_frame_xyz"])
+        segment_data["xyz"] = self.points_augmentation(segment_data["xyz"])
+
         return segment_data
     
+    def points_augmentation(self, points):
+ 
+        # standardization: zero-mean
+        if self.rotate_to_center:
+            _mean = points.mean(axis=0)
+            _theta = np.arctan2(_mean[1], _mean[0])
+            _rot = np.array([[ np.cos(_theta), np.sin(_theta)],
+                             [-np.sin(_theta), np.cos(_theta)]])
+            points[:,:2] = _rot.dot((points[:,:2] - _mean[None,:2]).T).T
+
+        if self.random_jitter:
+            # parameters taken from modelnet_dataset.py of the PointNet++ project
+            jitter = np.clip(0.01 * np.random.randn(points.shape[0], points.shape[1]), -0.05, 0.05)
+            points += jitter
+
+        if self.random_flip:
+            # NOTE: randomly flip point cloud by y-axis (velo cs)
+            # the intuition is that if we drive in the middle of a road
+            # we might see the same thing on either side of the road
+            # such phenomenon can be simulated using flipping
+            if np.random.rand() > 0.5:
+                points[:,1] = -points[:,1]
+
+        return points
+
     def collate_batch(self, batch):
         batch_size = len(batch)
         ans_dict = {}
